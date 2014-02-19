e737d99abc8cfee865cdc9d06c26298bc7598b90
Blixt <blixt@spotify.com>
2014-02-19 17:57:36 -0500

    Allow directory selection in Chrome

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 var AssetsManager = require('starbound-assets').AssetsManager;
 var WorldManager = require('starbound-world').WorldManager;
 var WorldRenderer = require('starbound-world').WorldRenderer;
@@ -11,18 +13,103 @@ var world = new WorldManager({workerPath: 'build/worker-world.js'});
 // Set up a renderer that will render the graphics onto screen.
 var renderer = new WorldRenderer(document.getElementById('viewport'), world, assets);
 
-document.starbounded.assets.onchange = function () {
-  assets.addFile('/', this.files[0], function (err) {
-    // TODO: Verify that packed.pak was chosen.
+function loadAssets(file) {
+  assets.addFile('/', file, function (err) {
     renderer.preload();
   });
-};
+}
 
-document.starbounded.world.onchange = function () {
-  world.open(this.files[0], function (err, metadata) {
+function loadWorld(file) {
+  world.open(file, function (err, metadata) {
     renderer.render();
   });
-};
+}
+
+var worldsAdded, groups = {};
+function addWorld(file) {
+  var list = document.starbounded.worldList;
+
+  if (!worldsAdded) {
+    // Remove the "Select directory" message.
+    list.remove(0);
+    list.removeAttribute('disabled');
+    document.starbounded.loadWorld.removeAttribute('disabled');
+    worldsAdded = {};
+  }
+
+  var pieces = file.name.replace('.world', '').split('_');
+  var sector = pieces[0];
+
+  var group = groups[sector];
+  if (!group) {
+    group = document.createElement('optgroup');
+    group.setAttribute('label', sector);
+    groups[sector] = group;
+    list.appendChild(group);
+  }
+
+  // Add the world in the right place according to time modified.
+  for (var i = 0; i < group.childNodes.length; i++) {
+    var other = worldsAdded[group.childNodes[i].value];
+    if (other.lastModifiedDate < file.lastModifiedDate) break;
+  }
+
+  worldsAdded[file.name] = file;
+
+  var label = 'planet ' + pieces[4];
+  if (pieces[5]) label += ' moon ' + pieces[5];
+  label += ' @ (' + pieces[1] + ', ' + pieces[2] + ')';
+  label += ', played ' + moment(file.lastModifiedDate).fromNow();
+
+  var option = new Option(label, file.name);
+  option.setAttribute('disabled', '');
+  group.insertBefore(option, group.childNodes[i]);
+
+  // Verify that the world file is valid.
+  var reader = new FileReader();
+  reader.onloadend = function () {
+    if (reader.result == 'SBBF02') {
+      option.removeAttribute('disabled');
+    } else {
+      option.label = '[pre-angry] ' + option.label;
+    }
+  };
+  reader.readAsText(file.slice(0, 6));
+}
+
+if (document.starbounded.root.webkitdirectory) {
+  // The browser supports selecting the directory.
+  document.body.classList.add('directory-support');
+
+  document.starbounded.root.onchange = function () {
+    for (var i = 0; i < this.files.length; i++) {
+      var file = this.files[i];
+      if (file.name == 'packed.pak') {
+        loadAssets(file);
+      } else if (file.name.match(/\.world$/)) {
+        addWorld(file);
+      }
+    }
+  };
+
+  document.starbounded.loadWorld.onclick = function () {
+    var file = worldsAdded && worldsAdded[document.starbounded.worldList.value];
+    if (!file) return;
+    loadWorld(file);
+
+    document.starbounded.loadWorld.setAttribute('disabled', '');
+    document.starbounded.worldList.setAttribute('disabled', '');
+  };
+} else {
+  // Separate files solution.
+  document.starbounded.assets.onchange = function () {
+    loadAssets(this.files[0]);
+  };
+
+  document.starbounded.world.onchange = function () {
+    loadWorld(this.files[0]);
+  };
+}
 
 document.onkeydown = function (event) {
   switch (event.keyCode) {
